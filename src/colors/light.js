58085ce4a79029229b45d3aaf86d9e02cf3ce52e
YenTe Ho <ythodesign@gmail.com>
2021-06-12 23:16:23 +0800

    color

--- src/colors/light.js
+++ src/colors/light.js
@@ -21,17 +21,8 @@ import {
   transparentize,
   toColorString,
 } from 'polished'
-const color_White = '#ffffff'
-const color_Black = '#000000'
-const primary = '#302E44' // > Luxury Purple
 
-const color_B1 = `#504B69`
-const color_B2 = `#BFB9C5`
-const color_B2_OP50 = `${transparentize(0.5, `${color_B2}`)}`
-const color_B2_OP70 = `${transparentize(0.3, `${color_B2}`)}`
-const color_B3 = `#27263A`
-
-// // const primary = '#263238' //900
+// const primary = '#263238' //900
 // const primary = '#0A365E' // > professional blue
 // const primary ='#0A3331';// > professional Green (10,51,49)
 // const primary = '#1F052C'// > luxury purple
@@ -50,87 +41,55 @@ const color_B3 = `#27263A`
 // const primary = '#80cbc4' //200
 // const primary = "#7986cb"; //300
 
-// desaturate
-const pDesaturate01 = `${desaturate('0.1', `${primary}`)}`
-// setSaturation
-const pSaturation05 = `${setSaturation(0.5, `${primary}`)}`
-const pSaturate08 = `${setSaturation(0.8, `${primary}`)}`
-// tint
-const pTint02 = `${tint(0.2, `${primary}`)}`
-const pTint04 = `${tint(0.4, `${primary}`)}`
-const pTint06 = `${tint(0.6, `${primary}`)}`
-const pTint08 = `${tint(0.8, `${primary}`)}`
-const pTint10 = `${tint(1.0, `${primary}`)}`
-// lighten
-const pLighten02 = `${lighten(0.1, `${primary}`)}`
-// darken
-const pDarken02 = `${darken(0.2, `${primary}`)}`
-const pDarken04 = `${darken(0.4, `${primary}`)}`
-const pDarken06 = `${darken(0.6, `${primary}`)}`
-const pDarken08 = `${darken(0.8, `${primary}`)}`
-//Transparent
-const pTransparent08 = `${transparentize(0.8, `${primary}`)}`
+const primary = '#302E44' // > Luxury Purple
 
-// invert
-const pInvert = `${invert(`${primary}`)}`
-//Gray
-const pGrayscale = `${grayscale(`${primary}`)};`
-
-const secondary = `${setHue('42', `${primary}`)}`
-
-// Text
-const textLightBg = `${pDarken04}`
-const textDarkBg = `${pTint06}`
-
-const textLightBgActive = `${invert(`${setHue('42', `${primary}`)}`)}`
-const textDarkBgActive = `${setLightness(0.6, `${setHue('42', `${primary}`)}`)}`
-
-const mixWhite20 = `${mix(0.2, '#fff', `${primary}`)}`
-const mixWhite80 = `${mix(0.8, '#fff', `${primary}`)}`
-const mixBlack5 = `${mix(0.05, '#000', `${primary}`)}`
-const mixBlack10 = `${mix(0.1, '#000', `${primary}`)}`
-const mixBlack60 = `${mix(0.6, '#000', `${primary}`)}`
-const mixBlack80 = `${mix(0.8, '#000', `${primary}`)}`
-const shadowLight = `${mix(0.8, `${primary}`, '#fff')}`
-const shadowDark = `${mix(0.8, `${primary}`, '#000')}`
-
+const color_White = '#ffffff'
+const color_Black = '#000000'
 const color_P1 = `${primary}`
-const color_P1_hover = `${pTint02}`
-const color_P1_active = `${mixBlack10}`
+const color_P1_hover = `${tint(0.2, `${primary}`)}`
+const color_P1_active = `${mix(0.1, '#000', `${primary}`)}`
 const color_P1_OP50 = `${transparentize(0.5, `${primary}`)}`
 const color_P1_OP70 = `${transparentize(0.3, `${primary}`)}`
 const color_P1_OP80 = `${transparentize(0.2, `${primary}`)}`
 const color_P1_OP90 = `${transparentize(0.1, `${primary}`)}`
 const color_P1_OP0 = `${transparentize(1, `${primary}`)}`
 const color_P2 = `${darken(0.1, `${invert(`${primary}`)}`)}`
+const color_B1 = `#504B69`
+const color_B2 = `#BFB9C5`
+const color_B2_OP50 = `${transparentize(0.5, `${color_B2}`)}`
+const color_B2_OP70 = `${transparentize(0.3, `${color_B2}`)}`
+const color_B2_hover = `${tint(0.2, `${color_B2}`)}`
+const color_B2_sctive = `${mix(0.1, '#000', `${color_B2}`)}`
+const color_B3 = `#27263A`
+
 const color_Shadow = `${readableColor(
   `${primary}`,
-  `${shadowDark}`,
+  `${mix(0.8, `${primary}`, '#000')}`,
   `rgba(0, 0, 0, 0.25)`,
   false
 )}`
 const color_T1 = `${readableColor(
   `${primary}`,
-  `${pDarken04}`,
-  `${pTint08}`,
+  `${darken(0.4, `${primary}`)}`,
+  `${tint(0.8, `${primary}`)}`,
   false
 )}`
 const color_T1_hover = `${readableColor(
   `${primary}`,
-  `${pDarken06}`,
-  `${pTint10}`,
+  `${darken(0.6, `${primary}`)}`,
+  `${tint(1.0, `${primary}`)}`,
   false
 )}`
 const color_T1_active = `${readableColor(
   `${primary}`,
-  `${pDarken06}`,
-  `${pTint10}`,
+  `${darken(0.6, `${primary}`)}`,
+  `${tint(1.0, `${primary}`)}`,
   false
 )}`
 const color_T2 = `${readableColor(
   `${primary}`,
-  `${textLightBgActive}`,
-  `${textDarkBgActive}`,
+  `${invert(`${setHue('42', `${primary}`)}`)}`,
+  `${setLightness(0.6, `${setHue('42', `${primary}`)}`)}`,
   false
 )}`
 
@@ -152,8 +111,53 @@ export default {
   color_B2_OP50,
   color_B2_OP70,
   color_B2,
+  color_B2_hover,
+  color_B2_sctive,
   color_B3,
   color_Shadow,
   color_White,
   color_Black,
 }
+
+// // desaturate
+// const pDesaturate01 = `${desaturate('0.1', `${primary}`)}`
+// // setSaturation
+// const pSaturation05 = `${setSaturation(0.5, `${primary}`)}`
+// const pSaturate08 = `${setSaturation(0.8, `${primary}`)}`
+// // tint
+// const pTint02 = `${tint(0.2, `${primary}`)}`
+// const pTint04 = `${tint(0.4, `${primary}`)}`
+// const pTint06 = `${tint(0.6, `${primary}`)}`
+// const pTint08 = `${tint(0.8, `${primary}`)}`
+// const pTint10 = `${tint(1.0, `${primary}`)}`
+// // lighten
+// const pLighten02 = `${lighten(0.1, `${primary}`)}`
+// // darken
+// const pDarken02 = `${darken(0.2, `${primary}`)}`
+// const pDarken04 = `${darken(0.4, `${primary}`)}`
+// const pDarken06 = `${darken(0.6, `${primary}`)}`
+// const pDarken08 = `${darken(0.8, `${primary}`)}`
+// //Transparent
+// const pTransparent08 = `${transparentize(0.8, `${primary}`)}`
+
+// // invert
+// const pInvert = `${invert(`${primary}`)}`
+// //Gray
+// const pGrayscale = `${grayscale(`${primary}`)};`
+
+// const secondary = `${setHue('42', `${primary}`)}`
+
+// // Text
+// const textLightBg = `${darken(0.4, `${primary}`)}`
+// const textDarkBg = `${tint(0.6, `${primary}`)}`
+
+// const textLightBgActive = `${invert(`${setHue('42', `${primary}`)}`)}`
+// const textDarkBgActive = `${setLightness(0.6, `${setHue('42', `${primary}`)}`)}`
+// const mixWhite20 = `${mix(0.2, '#fff', `${primary}`)}`
+// const mixWhite80 = `${mix(0.8, '#fff', `${primary}`)}`
+// const mixBlack5 = `${mix(0.05, '#000', `${primary}`)}`
+// const mixBlack10 = `${mix(0.1, '#000', `${primary}`)}`
+// const mixBlack60 = `${mix(0.6, '#000', `${primary}`)}`
+// const mixBlack80 = `${mix(0.8, '#000', `${primary}`)}`
+// const shadowLight = `${mix(0.8, `${primary}`, '#fff')}`
+// const shadowDark = `${mix(0.8, `${primary}`, '#000')}`
